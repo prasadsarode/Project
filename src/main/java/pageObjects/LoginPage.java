09611507a7552776a4572a5f44e6cfd87e6fedfe
Prasad <prasadsarode1@gmail.com>
2021-03-28 14:22:07 +0530

    modified login page

--- src/main/java/pageObjects/LoginPage.java
+++ src/main/java/pageObjects/LoginPage.java
@@ -3,7 +3,7 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.DataProvider;
+
 
 public class LoginPage {
 	
